2428425f9fc8f7722f0e8e3c04f7ce1aee1cdf1e
Wesley Ford <wes@wesdevpro.com>
2025-01-19 07:12:06 -0500

    chore: upgrade to `v0.2.0-next`

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <b-carousel>
+        <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+            <section :class="`hero is-fullheight is-${carousel.color}`">
+                <div class="hero-body has-text-centered">
+                   <div class="container has-text-centered">
+                     <h1 class="title">{{carousel.text}}</h1>
+                   </div>
+                </div>
+            </section>
+        </b-carousel-item>
+    </b-carousel>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { BCarousel, BCarouselItem } from 'buefy'
+
+export default defineComponent({
+    components: {
+        BCarousel,
+        BCarouselItem
+    },
+    data(){
+        return {
+            carousels: [
+                { text: 'Slide 1', color: 'primary' },
+                { text: 'Slide 2', color: 'info' },
+                { text: 'Slide 3', color: 'success' },
+                { text: 'Slide 4', color: 'warning' },
+                { text: 'Slide 5', color: 'danger' }
+            ]
+        }
+    }
+})
+</script>
